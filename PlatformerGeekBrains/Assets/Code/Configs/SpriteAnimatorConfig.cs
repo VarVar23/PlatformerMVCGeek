c67aac8b4d81ed605cba290f4b9e932f91658403
NooB <oleg.nakhaevgame@mail.ru>
2021-09-16 03:17:34 +0300

    Homework 1

---
+++ PlatformerGeekBrains/Assets/Code/Configs/SpriteAnimatorConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Platformer
+{
+    public enum AnimState
+    {
+        IDLE,
+        RUN,
+        JUMP
+    }
+
+    [CreateAssetMenu(fileName = "SpriteAnimatorConfig", menuName = "Configs/Animation", order = 1)]
+    public class SpriteAnimatorConfig : ScriptableObject
+    {
+        [Serializable]
+        public sealed class SpriteSequens
+        {
+            public AnimState Track;
+            public List<Sprite> Sprites = new List<Sprite>();
+        }
+
+        public List<SpriteSequens> Sequens = new List<SpriteSequens>();
+    }
+}
+
